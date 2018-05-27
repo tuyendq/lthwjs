d5409db1373feb2c2ad6dd74d869c77e6d6bfb47
Tuyen Dang <tuyend@gmail.com>
2018-05-27 07:09:59 +0700

    x220: add sumAll2.js

---
+++ sumAll2.js
@@ -0,0 +1,11 @@
+function sumAll(arr) {
+    var max = Math.max(...arr);
+    var min = Math.min(...arr);
+    var sum = 0;
+    for (var i = min; i <= max; i++){
+      sum += i;
+    }
+    return sum;
+  }
+  
+  sumAll([10, 5]);
