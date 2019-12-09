02b361ef6b6b07a7e82b2fae1c82b39cebc203a7
Tuyen Dang <tuyendq@gmail.com>
2019-12-09 20:24:21 +0700

    add index.html, functions.js, js.png

---
+++ functions.js
@@ -0,0 +1,47 @@
+'use strict'
+
+console.log("Hi")
+
+// global variable
+var a = 5
+
+function myFunction(){
+    a = 3
+    return a * a
+}
+
+console.log("a = " + a)
+console.log("Call myFunction(): " + myFunction())
+console.log("a = " + a)
+
+// initiate counter
+// var counter = 0
+// increment counter
+function add(){
+    var counter = 0
+    function plus(){
+        counter += 1
+    }
+    plus()
+    return counter
+}
+
+console.log("Counter is: " + add())
+
+console.log("Counter is: " + add())
+
+var add2 = (function () {
+    var counter = 0;
+    return function () { 
+        counter += 2
+        return counter
+    }
+})();
+
+console.log("Call add2 1st time: " + add2())
+console.log("Call add2 2nd time: " + add2())
+console.log("Call add2 3rd time: " + add2())
+
+console.log("Counter is: " + add())
+console.log("Counter is: " + add())
+console.log("Call add2 3rd time: " + add2())
