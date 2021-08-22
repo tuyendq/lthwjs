fd132cc40cc43524e0a3bd021cc17f470a577d81
Tuyen Dang <tuyendq@gmail.com>
2021-08-22 21:58:44 +0700

    palindrome.js: refactor following eslint

--- palindrone.js
+++ palindrone.js
@@ -1,17 +1,23 @@
+/* eslint-disable no-console */
+'use strict';
+
 function palindrome(str) {
-    // Good luck!
-    var nonAlphanumeric = /\s+|[.,\/#!$%\^&\*;:{}=\-_`~()]/gi;  
-    strTemp = str.replace(nonAlphanumeric, "");
-    strTemp = strTemp.toLowerCase();
-    if (strTemp === reverseString(strTemp)){
-      return true;
-    } else {
+  // Good luck!
+  const nonAlphanumeric = /\s+|[.,/#!$%\^&*;:{}=\-_`~()]/gi;  
+  let strTemp = str.replace(nonAlphanumeric, "");
+  strTemp = strTemp.toLowerCase();
+  if (strTemp === reverseString(strTemp)) {
+    return true;
+  } else {
     return false;
-    }
   }
+}
   
-  function reverseString(str){
-    var strTemp = str.split('');  
-    strTemp.reverse();
-    return strTemp.join('');
-  }
+function reverseString(str) {
+  let strTemp = str.split('');  
+  strTemp.reverse();
+  return strTemp.join('');
+}
+
+console.log(palindrome('anna'));
+console.log(palindrome('test'));
